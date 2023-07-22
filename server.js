fc72978383991cc14b9b1ab9e7e66dea72f1bd56
OrestShylan <orestshylan1997@gmail.com>
2023-07-22 18:11:30 +0300

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const DB_HOST =
-  "mongodb+srv://Orest:iXa821IJF5owWMlr@cluster0.dffktss.mongodb.net/pets";
+  "mongodb+srv://Orest:iXa821IJF5owWMlr@cluster0.dffktss.mongodb.net/pets?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
 
