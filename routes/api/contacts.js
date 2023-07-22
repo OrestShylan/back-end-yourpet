1d1e0bba5512d5813d3bb3fad35b838c0eba1157
OrestShylan <orestshylan1997@gmail.com>
2023-07-22 16:04:07 +0300

    add controller, change file server js, connect to data base

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,40 @@
 const express = require("express");
 
+const ctrl = require("../../controllers/contact");
+
+const {
+  validateBody,
+  isValidId,
+  validateFavorite,
+} = require("../../middleWares");
+
+const { schemas } = require("../../models/contact");
+
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/", ctrl.getAll);
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.get("/:contactId", isValidId, ctrl.getById);
 
-router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.post("/", ctrl.add);
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.post("/", validateBody(schemas.addschema), ctrl.add);
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
+router.delete("/:contactId", isValidId, ctrl.deleteById);
+
+router.put(
+  "/:contactId",
+  isValidId,
+  validateFavorite,
+  validateBody(schemas.addschema),
+  ctrl.update
+);
+router.patch(
+  "/:contactId/favorite",
+  isValidId,
+
+  validateBody(schemas.updateFavoriteSchema),
+  ctrl.updateFavorite
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Orest:iXa821IJF5owWMlr@cluster0.dffktss.mongodb.net/your-pet";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
