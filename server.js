93d5396f1f503d11ca86bbb38efd04fd1cc0509b
OrestShylan <orestshylan1997@gmail.com>
2023-07-23 15:27:59 +0300

    add .env

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const DB_HOST =
-  "mongodb+srv://Orest:iXa821IJF5owWMlr@cluster0.dffktss.mongodb.net/pets?retryWrites=true&w=majority";
+// const DB_HOST =
+//   "mongodb+srv://Orest:iXa821IJF5owWMlr@cluster0.dffktss.mongodb.net/pets?retryWrites=true&w=majority";
+
+const {DB_HOST} = process.env
 
 mongoose.set("strictQuery", true);
 
