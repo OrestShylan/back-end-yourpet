2d8227eb4e0456669e9a83ae3814dc2ef415172a
OrestShylan <orestshylan1997@gmail.com>
2023-07-23 15:55:37 +0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger.json");
 
