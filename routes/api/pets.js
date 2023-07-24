9670e01dd2a79388fe277b37a48bd42cdfe71d52
YuliiaKulyk13 <kulyk.yuliia13@gmail.com>
2023-07-24 16:31:54 -0400

    update pets routes

--- routes/api/pets.js
+++ routes/api/pets.js
@@ -15,27 +15,12 @@ const router = express.Router();
 
 router.get("/", authenticate, ctrl.getAllPets);
 
-router.get("/:contactId", isValidId, ctrl.getById);
-
-router.post("/", ctrl.addPet);
-
-router.post("/", validateBody(schemas.addschema), ctrl.addPet);
-
-router.delete("/:id", isValidId, ctrl.deletePet);
-
-router.put(
-  "/:contactId",
-  isValidId,
-  validateFavorite,
-  validateBody(schemas.addschema),
-  ctrl.update
-);
-router.patch(
-  "/:contactId/favorite",
-  isValidId,
-
-  validateBody(schemas.updateFavoriteSchema),
-  ctrl.updateFavorite
+router.post(
+  "/",
+  authenticate,
+  validateBody(addPetJoiSchema),
+  uploadCloud.single("pets-photo"),
+  ctrl.addPet
 );
 
 router.delete("/:id", authenticate, isValidId, ctrl.deletePet);
