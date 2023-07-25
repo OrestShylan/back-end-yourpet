44176fc6618e2a58a503705d852544bd68f61663
AlexAndriushchenko <aleksandr.work3@gmail.com>
2023-07-25 09:50:39 +0100

    add /api/ = /api/users

--- services/authService.js
+++ services/authService.js
@@ -3,14 +3,14 @@ const User = require("../models/userModel");
 const RequestError = require("../helpers/RequestError");
 require("dotenv").config();
 
-// const signToken = (id) =>
-//   jwt.sign({ id }, process.env.JWT_SECRET, {
-//     expiresIn: process.env.JWT_EXPIRES_IN,
-//   });
 const signToken = (id) =>
-  jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
-    expiresIn: `${process.env.JWT_EXPIRES_IN}`,
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
+// const signToken = (id) =>
+//   jwt.sign({ id }, `${process.env.JWT_SECRET}`, {
+//     expiresIn: `${process.env.JWT_EXPIRES_IN}`,
+//   });
 
 const register = async (name, email, password) => {
   const newUser = await User.create({
